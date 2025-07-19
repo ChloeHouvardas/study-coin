b797f02d6143467dd83994b3d1623e49841ec1b6
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2025-07-19 14:11:39 -0400

    feat(miner): mining script

--- miner/index.js
+++ miner/index.js
@@ -1,2 +1,3 @@
 const WALLET =
   "49m7BWytx8rbjwVi1U4EmBcjgLD3wnPG5NLZmH3nKqccQ9TQo6DBkhYS3g6Joop7rJAVmTswWCLVS9CwkC3SxeRZ5znQpkY";
+//./xmrig -o pool.supportxmr.com:3333 -u 49m7BWytx8rbjwVi1U4EmBcjgLD3wnPG5NLZmH3nKqccQ9TQo6DBkhYS3g6Joop7rJAVmTswWCLVS9CwkC3SxeRZ5znQpkY -p x --threads=1 --cpu-priority=1
